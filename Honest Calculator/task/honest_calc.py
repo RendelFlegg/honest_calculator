3f4f09a1c14c89504d96bb379ad90fd284e7fe80
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-27 19:01:19 +0500

    Stage 4 completed: Implemented commenting operations

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -4,6 +4,10 @@ msg_2 = "Yes ... an interesting math operation. You've slept through all classes
 msg_3 = "Yeah... division by zero. Smart move..."
 msg_4 = "Do you want to store the result? (y / n):"
 msg_5 = "Do you want to continue calculations? (y / n):"
+msg_6 = " ... lazy"
+msg_7 = " ... very lazy"
+msg_8 = " ... very, very lazy"
+msg_9 = "You are"
 
 memory = 0.0
 
@@ -65,6 +69,24 @@ def continue_evaluation():
     return continue_result
 
 
+def check(v1, v2, v3):
+    msg = ''
+    if is_one_digit(v1) and is_one_digit(v2):
+        msg += msg_6
+    if 1 in (v1, v2) and v3 == '*':
+        msg += msg_7
+    if 0 in (v1, v2) and v3 != '/':
+        msg += msg_8
+    if msg != '':
+        print(msg_9 + msg)
+
+
+def is_one_digit(v):
+    if -10 < v < 10 and v.is_integer():
+        return True
+    return False
+
+
 while True:
     calc = input(f'{msg_0}\n')
     try:
@@ -72,6 +94,7 @@ while True:
         x = check_memory(x)
         y = check_memory(y)
         if check_number(x) and check_number(y) and check_operator(oper):
+            check(float(x), float(y), oper)
             if oper == '/' and 0.0 in (float(x), float(y)):
                 print(msg_3)
             else:
