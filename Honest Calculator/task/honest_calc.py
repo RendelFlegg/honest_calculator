7cf9104f6a0fbc6ba259d45e9c883f28fbde1a90
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-27 16:42:00 +0500

    Stage 1 completed: Implemented data collection

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,1 +1,30 @@
-# write your code here
+msg_0 = "Enter an equation"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+
+
+def check_number(number):
+    try:
+        float(number)
+        return True
+    except ValueError:
+        print(msg_1)
+        return False
+
+
+def check_operator(operator):
+    if operator in ('+', '-', '*', '/'):
+        return True
+    else:
+        print(msg_2)
+        return False
+
+
+while True:
+    calc = input(f'{msg_0}\n')
+    try:
+        x, oper, y = calc.split()
+        if check_number(x) and check_number(y) and check_operator(oper):
+            break
+    except ValueError:
+        continue
