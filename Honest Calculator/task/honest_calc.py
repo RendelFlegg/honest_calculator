29a03aa4cbca032e1ce802d8e635a86914810ef0
RendelFlegg <62382457+RendelFlegg@users.noreply.github.com>
2022-01-27 16:52:54 +0500

    Stage 2 completed: Implemented basic calculations

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,6 +1,7 @@
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
 
 
 def check_number(number):
@@ -20,11 +21,26 @@ def check_operator(operator):
         return False
 
 
+def evaluation_result(first_number, operator, second_number):
+    if operator == '+':
+        return first_number + second_number
+    elif operator == '-':
+        return first_number - second_number
+    elif operator == '*':
+        return first_number * second_number
+    else:
+        return first_number / second_number
+
+
 while True:
     calc = input(f'{msg_0}\n')
     try:
         x, oper, y = calc.split()
         if check_number(x) and check_number(y) and check_operator(oper):
-            break
+            if oper == '/' and 0 in (int(x), int(y)):
+                print(msg_3)
+            else:
+                print(evaluation_result(float(x), oper, float(y)))
+                break
     except ValueError:
         continue
